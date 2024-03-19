1003474c5cb1b0bf907b53fb1d0dc3fb10363b4d
Yongeun Kwon <ykwon35@my.bcit.ca>
2024-03-19 13:37:23 -0700

    Added all pet functionalities

--- routes/router.js
+++ routes/router.js
@@ -101,7 +101,7 @@ router.get("/showPets", async (req, res) => {
       console.log(pets);
       let owner = await pets[0].getOwner();
       console.log(owner);
-      res.render("pets", { allPets: pets });
+      res.render("pet", { allPets: pets });
     }
   } catch (ex) {
     res.render("error", { message: "Error connecting to MySQL" });
